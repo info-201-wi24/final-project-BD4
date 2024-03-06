248322d1fda998e57ffdd2220ef39a311b3baebf
ntsegay03 <159969434+ntsegay03@users.noreply.github.com>
2024-03-05 18:30:50 -0800

    added vis 2 server update

--- server.R
+++ server.R
@@ -21,5 +21,23 @@ server <- function(input, output){
         labs(title = "Distribution of Energy Level per Song by Country")
     return(ggplotly(my_plot))
   })
+
+output$sng_vs_alb_out <- renderPlotly({
+
+    filtered_data <- dataset
+    if (input$sng_vs_alb_select != "Both") {
+      filtered_data <- dataset[dataset$is_single == (input$sng_vs_alb_select == "Single"), ]
+    }
+    
+    p <- ggplot(filtered_data, aes(x = Followers, y = popularity)) +
+      geom_point(aes(color = as.factor(is_single)), alpha = 0.5) +  
+      scale_color_manual(values = c("TRUE" = "darkgreen", "FALSE" = "darkgrey"), labels = c("Single", "Album")) +
+      labs(title = "Song Popularity vs. Artist Followers", x = "Number of Followers", y = "Popularity") +
+      theme_minimal() +
+      scale_x_log10() +  
+      theme(axis.text.x = element_text(angle = 45, hjust = 1))  
+    
+    ggplotly(p)
+  })
   
 }
